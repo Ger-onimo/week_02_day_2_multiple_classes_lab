78ffef46adc95e717f687bd60696b3a48d8a3a50
Jamie Reid <wjreid85.jr@gmail.com>
2018-11-27 15:04:21 +0000

    Add bus destination test pass

--- bus.rb
+++ bus.rb
@@ -1,17 +1,21 @@
 
 class Bus
 
-  attr_reader :route_number
+  attr_reader :route_number, :destination
 
-  def initialize(route_number) #destination)
+  def initialize(route_number, destination)
 
     @route_number = route_number
-    #@destination = destination
+    @destination = destination
 
   end
 
   # def route_number()
   #   return @route_number
+  # end
+
+  # def destination()
+  #   return @destination
   # end
 
 
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -9,8 +9,13 @@ class BusTest < MiniTest::Test
   end
 
   def test_bus_has_a_route()
-    bus = Bus.new(22) #{}"Ocean Terminal")
+    bus = Bus.new(22, "Ocean Terminal")
     assert_equal(22, bus.route_number)
+  end
+
+  def test_bus_has_a_destination()
+    bus = Bus.new(22, "Ocean Terminal")
+    assert_equal("Ocean Terminal", bus.destination)
   end
 
 
